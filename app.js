06cb090338b83f1c5d18dc1ea95cdea371897db9
Nate Notermann <nate.notermann@gmail.com>
2022-10-29 00:05:21 -0500

    finished build, bug with sound and depreciations issues

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
+import * as Tone from 'tone'
+
 function sequencer() {
 
-    const claps = new Tone.Player('./SFX/tom-808.wav').toDestination();
-    const snare = new Tone.player('./SFX/clap-analog.wav').toDestination();
+    const claps = new Tone.Player('./SFX/clap-analog.wav').toDestination();
+    const snare = new Tone.Synth('./SFX/snare-pinch.wav').toDestination();
+    const now = Tone.now()
+    console.log(claps);
+
     let index = 0;
 
     Tone.Transport.scheduleRepeat(repeat, '8n' );
-    Tone.transport.start();
+    Tone.Transport.start();
 
     function repeat(){
         let step = index % 8;
-        let seclectedSfx = document.querySelector(`.top input:nth-child(${step+ 1}`);
-        let seclectedSfx2 = document.querySelector(`.bottom input:nth-child(${step+ 1}`);
-        if (seclectedSfx.checked) {
+        let selectedSfx = document.querySelector(`.top input:nth-child(${step+ 1}`);
+        let selectedSfx2 = document.querySelector(`.bottom input:nth-child(${step+ 1}`);
+        if (selectedSfx.checked) {
             claps.start();
         }
-        if (seclectedSfx2.checked) {
+        if (selectedSfx2.checked) {
             snare.start();
         }
         index ++
     }
 
+    
 }
-
 
 sequencer();
